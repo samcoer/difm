c91955cf0d070569b6337deb9151ad525f6aadbf
Nizam <nizam.lko@gmail.com>
2015-06-21 15:44:43 +0530

    order service html

--- difm_web/src/info/difm/inte/service/OrderController.java
+++ difm_web/src/info/difm/inte/service/OrderController.java
@@ -2,7 +2,6 @@ package info.difm.inte.service;
 
 import info.difm.biz.service.OrderService;
 import info.difm.db.bo.Order;
-import info.difm.db.bo.UserProfile;
 
 import java.util.List;
 import java.util.Map;
@@ -11,14 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
  
 @Controller
 @RequestMapping(value = "/order")
